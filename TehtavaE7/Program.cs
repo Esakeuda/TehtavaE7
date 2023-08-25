d216db10d7fe3d7324029029079f816826f7b3c7
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-25 09:06:47 +0300

    Add project files.

---
+++ TehtavaE7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KolmosellaLuvut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Käydään läpi kaikki luvut väliltä 1-99
+            for (int luku = 1; luku < 100; luku++)
+            {
+                // Tarkistetaan, onko luku jaollinen kolmella
+                if (luku % 3 == 0)
+                {
+                    Console.WriteLine(luku);  // Tulostetaan luku, jos se on jaollinen kolmella
+                }
+            }
+        }
+    }
+}
+
